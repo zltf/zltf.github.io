9585b1de84ae3c1b6ad914ea621b7411861001bd
zltf <1274803758@qq.com>
2016-11-27 02:01:10 +0800

    修改像素鸟bug

--- js/game.js
+++ js/game.js
@@ -127,6 +127,7 @@ game.States.play = function(){
 	};
 
 	this.fly = function(){
+		if(this.bird.body.y>0)
 		this.bird.body.velocity.y = -350;
 		game.add.tween(this.bird).to({angle:-30}, 100, null, true, 0, 0, false); //上升时头朝上
 		this.soundFly.play();
